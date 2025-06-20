e54ff64b2060b4bd9b1755387a74533b1399dbb7
SantiagoRomero7 <santiagoinstagram23@gmail.com>
2025-06-20 12:03:23 -0500

    feat: :sparkles: arreglos y medidas responsivas
    arreglos y medidas responsivas del carrusel

--- productos.js
+++ productos.js
@@ -1,25 +1,29 @@
-
 document.addEventListener('DOMContentLoaded', () => {
     const imagenes = document.querySelectorAll('.carousel img');
     const btnPrev = document.querySelector('.prev');
     const btnNext = document.querySelector('.next');
-    let indiceActual = 0;
+    let actual = 0;
     let intervalo;
   
     function mostrarImagen(index) {
       imagenes.forEach((img, i) => {
-        img.classList.toggle('active', i === index);
+        if (i === index) {
+          img.classList.add('active');
+          gsap.fromTo(img, {opacity: 0}, {opacity: 1, duration: 1});
+        } else {
+          img.classList.remove('active');
+        }
       });
     }
   
     function siguienteImagen() {
-      indiceActual = (indiceActual + 1) % imagenes.length;
-      mostrarImagen(indiceActual);
+      actual = (actual + 1) % imagenes.length;
+      mostrarImagen(actual);
     }
   
     function anteriorImagen() {
-      indiceActual = (indiceActual - 1 + imagenes.length) % imagenes.length;
-      mostrarImagen(indiceActual);
+      actual = (actual - 1 + imagenes.length) % imagenes.length;
+      mostrarImagen(actual);
     }
   
     btnNext.addEventListener('click', () => {
@@ -41,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
       iniciarIntervalo();
     }
   
-    mostrarImagen(indiceActual);
+    mostrarImagen(actual);
     iniciarIntervalo();
   });
   
