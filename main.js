e6e84024254ed5410972fd3da7b2888edee70a86
SantiagoRomero7 <santiagoinstagram23@gmail.com>
2025-06-20 08:33:03 -0500

    feat: :sparkles: arreglos del js
    arreglos del js

--- main.js
+++ main.js
@@ -14,22 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
       ease: "power2.out"
     });
   
-    gsap.from(".eslogan", {
-      x: 40,
-      opacity: 0,
-      delay: 0.8,
-      duration: 1.2,
-      ease: "power2.out"
-    });
-  
-    gsap.from("#entrar-btn", {
-      scale: 0.8,
-      opacity: 0,
-      delay: 1.2,
-      duration: 1,
-      ease: "back.out(1.7)"
-    });
-  
     gsap.to(".texto h1", {
       y: -10,
       duration: 1.5,
@@ -46,20 +30,5 @@ document.addEventListener("DOMContentLoaded", () => {
       ease: "sine.inOut"
     });
 
-    gsap.to(".eslogan", {
-      opacity: 0.8,
-      duration: 3,
-      repeat: -1,
-      yoyo: true,
-      ease: "power1.inOut"
     });
-  
-    gsap.to("#entrar-btn", {
-      boxShadow: "0 0 15px #ffd700",
-      duration: 2,
-      repeat: -1,
-      yoyo: true,
-      ease: "sine.inOut"
-    });
-  });
   
